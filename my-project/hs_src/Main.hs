5794036a2c9c3f07d56adcd2e16f913ca4b4c8dd
1inguini <9647142@gmail.com>
2019-03-27 21:10:41 +0900

    separated c call

--- my-project/hs_src/Main.hs
+++ my-project/hs_src/Main.hs
@@ -1,29 +1,19 @@
 module Main where
 
-import           Data.Word
-
-foreign import ccall "mylib.h gpio_setup" gpio_setup :: IO ()
-foreign import ccall "mylib.h wait" wait :: Word -> IO ()
-foreign import ccall "mylib.h gpioB" c_GPIOB :: IO Word32
-foreign import ccall "mylib.h gpio12" c_GPIO12 :: IO Word16
-foreign import ccall "libopencm3/stm32/gpio.h gpio_set" c_gpio_set :: Word32 -> Word16 -> IO ()
-foreign import ccall "libopencm3/stm32/gpio.h gpio_clear" c_gpio_clear :: Word32 -> Word16 -> IO ()
-
-type Port = Word32
-type Pin = Word16
+import           Ccall as C
 
 blink :: Port -> Pin ->IO()
 blink port pin = do
   wait 100000
-  c_gpio_clear port pin
+  C.gpio_clear port pin
   wait 100000
-  c_gpio_set port pin
+  C.gpio_set port pin
 
   blink port pin
 
 main :: IO ()
 main = do
-  gpioB <- c_GPIOB
-  gpio12 <- c_GPIO12
-  gpio_setup
+  gpioB <- C.gpioB
+  gpio12 <- C.gpio12
+  C.gpio_setup
   blink gpioB gpio12
