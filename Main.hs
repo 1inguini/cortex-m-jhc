2be78e6a42b9b9a147fa5be1b6b6cf918940ba2f
1inguini <9647142@gmail.com>
2019-04-07 13:10:02 +0900

    made Haskell more bare

--- Main.hs
+++ Main.hs
@@ -5,15 +5,49 @@ import           Lib.Ccall as C
 blink :: Port -> Pin -> IO ()
 blink port pin = do
   wait 100000
-  C.gpio_clear port pin
+  C.gpioClear port pin
   wait 100000
-  C.gpio_set port pin
+  C.gpioSet port pin
 
   blink port pin
 
+onOff :: Port -> Pin -> Port -> Pin -> IO ()
+onOff inPort inPin outPort outPin = do
+  state <- C.gpioGet inPort inPin
+  if state == 0
+    then C.gpioClear outPort outPin
+    else C.gpioSet   outPort outPin
+
 main :: IO ()
 main = do
-  gpioB <- C.gpioB
+  C.rccPeriphClockEnableGpioA
+  C.rccPeriphClockEnableGpioB
+  C.rccPeriphClockEnableUsart2
+
+  gpioB  <- C.gpioB
   gpio12 <- C.gpio12
-  C.gpio_setup
+
+  gpioA  <- C.gpioA
+  gpio0  <- C.gpio0
+
+  gpioModeOutput2Mhz     <- C.gpioModeOutput2Mhz
+  gpioCnfOutputPushpull  <- C.gpioCnfOutputPushpull
+
+  gpioModeInput          <- C.gpioModeInput
+  gpioCnfInputPullUpdown <- C.gpioCnfInputPullUpdown
+
+  C.gpioSetMode
+    gpioB
+    gpioModeOutput2Mhz
+    gpioCnfOutputPushpull
+    gpio12
+
+  C.gpioSetMode
+    gpioA
+    gpioModeInput
+    gpioCnfInputPullUpdown
+    gpio0
+
   blink gpioB gpio12
+
+  -- onOff gpioA gpio0 gpioB gpio12
